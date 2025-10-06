e23e55b2cfff2452466818226b23958f9eade3ed
Taha-M-Fawy <tahafawy2@gmail.com>
2025-10-06 20:30:02 +0300

    add page the Teachers

---
+++ Teachers/Teacher.js
@@ -0,0 +1,27 @@
+async function GetDataTeachers() {
+  try {
+    const res = await fetch("../data/teachers.json");
+    const data = await res.json();
+
+    Object.keys(data).forEach(programKey => {
+      const section = document.querySelector(`.${programKey} .row`);
+
+      if (section) {
+        data[programKey].forEach(teacher => {
+          section.innerHTML += `
+            <div class="card">
+              <img class="imageTeacher" src="${teacher.image}" alt="${teacher.name}">
+              <h2>${teacher.name}</h2>
+              <p>${teacher.description}<br><strong>${teacher.specialization}</strong></p>
+              <button class="btn">Know More</button>
+            </div>
+          `;
+        });
+      }
+    });
+  } catch (err) {
+    console.error("Error loading teacher data:", err);
+  }
+}
+
+GetDataTeachers();
